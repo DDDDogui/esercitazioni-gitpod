8f3faf8bf7d0acd03f1857ae5ad041fe989d7839
DDDDogui <guido.carlozzo04@gmail.com>
2021-11-11 11:04:57 +0000

    canvas e dom

---
+++ Canvas e DOM/scriptCanvas.js
@@ -0,0 +1,9 @@
+console.log('Se leggi questo significa che la prova canvas e DOM sta funzionando (per ora) ok me la sono gufata')
+function button1Function() {
+    document.getElementById("mytext").innerHTML = "Hello World";
+    document.getElementById("mytext2").innerHTML = "Hello World part 2";  
+}
+function button2Function() {
+    document.getElementById("thirdtext").innerHTML = "Mi piace js";
+    document.getElementById("fourthtext").innerHTML = "A me non piace js";
+}
