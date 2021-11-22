2dadff04e6bc50106e17373b42de6c2e2604b7fa
DDDDogui <guido.carlozzo04@gmail.com>
2021-11-22 11:19:04 +0000

    versione 1 PrimaSpirale

--- PreparazioneVerifica/script.js
+++ PreparazioneVerifica/script.js
@@ -4,11 +4,23 @@ function buttonFunction() {
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(0, 0, 150, 75);
     ctx.fillStyle = "#00FF00";
-  
     ctx.fillRect(0, 0, 10, 10);
-  
     ctx.fillRect(20, 20, 10, 10);
   }
   function button2Function() {
-      
+    console.log("prova");
+    buttonFunction()
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#00FF00";
+    ctx.fillRect(40, 40, 10, 10);
+    ctx.fillRect(40, 60, 10, 10);
+    ctx.fillRect(60, 40, 10, 10);
+    ctx.fillRect(60, 60, 10, 10);
+    ctx.fillRect(0, 100, 10, 10);
+    ctx.fillRect(20, 80, 10, 10);
+    ctx.fillRect(100, 0, 10, 10);
+    ctx.fillRect(80, 20, 10, 10);
+    ctx.fillRect(100, 100, 10, 10);
+    ctx.fillRect(80, 80, 10, 10);
   }
