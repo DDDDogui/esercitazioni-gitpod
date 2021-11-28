a5687770b33bf6049355348ce8ec474212e5bbc2
DDDDogui <guido.carlozzo04@gmail.com>
2021-11-28 17:56:24 +0000

    terza versione con metodi

--- oggettiJS/script.js
+++ oggettiJS/script.js
@@ -1,14 +1,14 @@
-const animale1 = {species: "mucca", race: "chianina", paws: "4"};
-const animale2 = {species: "cane", race: "bassotto", paws: "4"};
-const animale3 = {species: "gallina", race: "andalusa", paws: "2"};
+const animale1 = {species: "mucca", race: "chianina", paws: 4, caratteristicheComplete: function() {return this.species + ", "+ this.race +", con "+ this.paws+ " zampe.";} };
+const animale2 = {species: "cane", race: "bassotto", paws: 4, caratteristicheComplete: function() {return this.species +", "+ this.race +", con "+ this.paws+ " zampe.";} };
+const animale3 = {species: "gallina", race: "andalusa", paws: 2, caratteristicheComplete: function() {return this.species +", "+ this.race +", con "+ this.paws+" zampe.";} };
 console.log("prova");
 function animal1() {
-    document.getElementById("firstButton").innerHTML = "Ecco le caratteristiche del primo animale: " + animale1.species +", " + animale1.race +", con " + animale1.paws+ " zampe.";
+    document.getElementById("firstButton").innerHTML = "Ecco le caratteristiche del primo animale: " + animale1.caratteristicheComplete();
     console.log("prova2");
 }
 function animal2() {
-    document.getElementById("firstButton").innerHTML = "Ecco le caratteristiche del secondo animale: " + animale2.species +", " + animale2.race +", con " + animale2.paws+ " zampe.";
+    document.getElementById("firstButton").innerHTML = "Ecco le caratteristiche del secondo animale: " + animale2.caratteristicheComplete();
 }
 function animal3() {
-    document.getElementById("firstButton").innerHTML = "Ecco le caratteristiche del terzo animale: " + animale3["species"] +", " + animale3["race"] +", con " + animale3["paws"] + " zampe.";
+    document.getElementById("firstButton").innerHTML = "Ecco le caratteristiche del terzo animale: " + animale3.caratteristicheComplete();
 }
